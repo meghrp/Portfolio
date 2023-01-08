fc771deb4f0d587007a1bb1f2974f58ea006c866
Megh Pathak <meghrp27@gmail.com>
2023-01-07 23:23:22 -0500

    Added dark mode

--- portfolio/src/components/Intro.jsx
+++ portfolio/src/components/Intro.jsx
@@ -3,11 +3,15 @@ import React from "react";
 function Intro() {
     return (
         <div className="flex items-center justify-center flex-col text-center pt-20 pd-6">
-            <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Megh Pathak</h1>
-            <p className="text-base md:text-xl mb-3 font-medium">Student and Gamer</p>
+            <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">
+                Megh Pathak
+            </h1>
+            <p className="text-base md:text-xl mb-3 font-medium">
+                Student and Gamer
+            </p>
             <p className="text-sm max-w-xl mb-6 font-bold">Bio</p>
         </div>
-    )
+    );
 }
 
 export default Intro;
