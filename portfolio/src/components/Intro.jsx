6a1664a5c8daf37085e10b4f9c6a2b506532e758
Megh Pathak <meghrp27@gmail.com>
2023-01-07 21:55:53 -0500

    Added info to intro section

--- portfolio/src/components/Intro.jsx
+++ portfolio/src/components/Intro.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 function Intro() {
     return (
-        <div>Intro</div>
+        <div className="flex items-center justify-center flex-col text-center pt-20 pd-6">
+            <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Megh Pathak</h1>
+            <p className="text-base md:text-xl mb-3 font-medium">Student and Gamer</p>
+            <p className="text-sm max-w-xl mb-6 font-bold">Bio</p>
+        </div>
     )
 }
 
