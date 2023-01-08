1a8c8ce28ec0acd1bf028c2ffec223fa29ea298f
Megh Pathak <meghrp27@gmail.com>
2023-01-07 22:45:57 -0500

    Timeline info section

--- portfolio/src/components/Timeline.jsx
+++ portfolio/src/components/Timeline.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import timeline from "../data/timeline.js";
+import TimelineItem from "./TimelineItem";
 
 function Timeline() {
-    return <div></div>;
+    return (
+        <div className="flex flex-col md:flex-row justify-center my-20">
+            <div className="w-full md:w-7/12">
+                {timeline.map((item) => (
+                    <TimelineItem
+                        year={item.year}
+                        title={item.title}
+                        duration={item.duration}
+                        details={item.details}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default Timeline;
---
+++ portfolio/src/data/timeline.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        year: "Today",
+        title: "HBSc in Computer Science and Mathematics",
+        duration: "2020 - 2025",
+        details: "blah blah blah",
+    },
+    {
+        year: "Today",
+        title: "Software Engineer",
+        duration: "2022 - Present",
+        details: "blah blah blah",
+    },
+    {
+        year: "2021",
+        title: "Sales Associate",
+        duration: "5 months",
+        details: "blah blah blah",
+    },
+];
