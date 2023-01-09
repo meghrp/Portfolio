21c36bde32f6c84569f0258a2cc520f3265102af
Megh Pathak <meghrp27@gmail.com>
2023-01-09 14:16:28 -0500

    Updated image paths

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
         title: "CodeGPT",
-        imgURL: "public/assets/codegpt.jpg",
+        imgURL: "assets/codegpt.jpg",
         stack: ["HTML", "CSS", "JavaScript"],
         link: "http://codegpt.vercel.app",
     },
     {
         title: "Portfolio",
-        imgURL: "public/assets/dev-portfolio.jpg",
+        imgURL: "assets/dev-portfolio.jpg",
         stack: ["React", "Tailwind", "Vite"],
         link: "",
     },
     {
         title: "Weather App",
-        imgURL: "public/assets/weatherapp.jpg",
+        imgURL: "assets/weatherapp.jpg",
         stack: ["HTML", "CSS", "JavaScript"],
         link: "https://meghrp.github.io/SimpleWeatherApp/",
     },
