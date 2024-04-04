78ca242b083ca75da83f030f5a812e4f1248db02
meghpathak <meghrp27@gmail.com>
2024-04-04 14:53:07 -0400

    Updated timeline

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -2,23 +2,21 @@ export default [
     {
         year: "Today",
         title: "HBSc in Computer Science and Mathematics",
-        duration: "2020 - 2025",
+        duration: "2020 - Present",
         details:
             "I'm pursuing the knowledge that a computer science degree has to offer. I'm learning about computer science theory, complex data structures and algorithms and how to apply them to real world problems. I'm also learning about the mathematics that underpins computer science and how to apply it to solve problems.",
     },
     {
         year: "Today",
-        title: "Software Engineer at ParkinSense",
-        duration: "2022 - Present",
-        details:
-            "I'm helping build an Android app for a startup under the Uoft Hatchery umbrella in an Agile team setting. I'm working on the backend of the app on Google Cloud Platform using python. I redesigned the database scheme to improve performance and scalability.",
+        title: "Technical Co-founder at ParkinSense",
+        duration: "2023 - Present",
+        details: "",
     },
     {
-        year: "2021",
-        title: "Sales Associate",
-        duration: "2 months",
-        details:
-            "Currently working at Staples as a Tech Associate providing technical suport and sales assistance to customers. I am skilled in troubleshooting hardware and software issues, as well as providing customer service. ",
+        year: "2022",
+        title: "Software Engineer at ParkinSense",
+        duration: "2022 - 2023",
+        details: "Built an Android app and implemented a serverless backend architecture for a startup offering continuous monitoring for Parkinson's patients.",
     },
     {
         year: "2018",
