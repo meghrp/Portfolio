7d5bf809efa3eddb1424cc824d16f1cff00eae6f
meghrp <meghrp27@gmail.com>
2024-09-10 22:06:29 -0400

    updated intro and timeline descriptions

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -4,25 +4,25 @@ export default [
         title: "HBSc in Computer Science and Mathematics",
         duration: "2020 - Present",
         details:
-            "I'm pursuing the knowledge that a computer science degree has to offer. I'm learning about computer science theory, complex data structures and algorithms and how to apply them to real world problems. I'm also learning about the mathematics that underpins computer science and how to apply it to solve problems.",
+            "Passionate about solving complex problems and implementing real-world solutions. Co-developed Athena Learning, automating student-project matchmaking for UofT courses, showcasing my drive to streamline processes and enhance user experiences.",
     },
     {
         year: "Today",
         title: "Technical Co-founder at ParkinSense",
         duration: "2023 - Present",
-        details: "",
+        details: "Developing wearable tech and software to help Parkinson's patients monitor and manage symptoms, focusing on practical solutions that impact patients' lives.",
     },
     {
         year: "2022",
         title: "Software Engineer at ParkinSense",
         duration: "2022 - 2023",
-        details: "Built an Android app and implemented a serverless backend architecture for a startup offering continuous monitoring for Parkinson's patients.",
+        details: "Developed an Android app and implemented serverless backend for a startup providing continuous monitoring for Parkinson's patients.",
     },
     {
         year: "2018",
         title: "First Major Project",
         duration: "",
         details:
-            "Along with a group of friends, we created a online voting app called Pollify. It was built using the MERN stack and was deployed locally. It was a great learning experience and I learned a lot about web development.",
+            "Created Pollify, an online voting app, with friends using the MERN stack. Deployed locally, it provided valuable web development experience.",
     },
 ];
