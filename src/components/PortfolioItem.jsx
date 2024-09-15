2fc13bb1eb3292e43ea5896e2e0818840d45e3e1
meghrp <meghrp27@gmail.com>
2024-09-15 00:43:27 -0400

    mid changes still fixing

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-function PortfolioItem({ title, imgURL, stack, link }) {
+export default function PortfolioItem({ title, imgURL, stack, link }) {
 	return (
 		<a
 			href={link}
 			target="_blank"
 			rel="noreferrer noopener"
-			className="border-2 border-gray-900 dark:border-gray-100 rounded-md duration-300 hover:scale-105 hover:shadow-[0_0_40px_rgba(0,0,255,0.5)]"
+			className="block w-full h-full border-2 border-gray-900 dark:border-gray-100 rounded-md duration-300 hover:scale-105 hover:shadow-[0_0_40px_rgba(0,0,255,0.5)]"
 		>
-			<img src={imgURL} alt="portfolio" className="w-full h-36 md:h-48 object-cover cursor-pointer" />
+			<img src={imgURL} alt="portfolio" className="w-full h-48 object-cover cursor-pointer" />
 			<div className="w-full p-4">
 				<h3 className="text-lg md:text-xl dark:text-white mb-2 md:mb-3 font-semibold">{title}</h3>
 				<p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-white">
@@ -25,5 +25,3 @@ function PortfolioItem({ title, imgURL, stack, link }) {
 		</a>
 	);
 }
-
-export default PortfolioItem;
